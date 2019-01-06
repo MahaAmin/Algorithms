ddee5a8151c65fc1a38183d2241f00302fc01d33
MahaAmin <mahaamin97@gmail.com>
2019-01-06 13:06:12 +0200

    Add FIB.py

--- Problem_Solving/Rosalind/FIB.py
+++ Problem_Solving/Rosalind/FIB.py
@@ -19,4 +19,16 @@ def FibCustom(n, k):
     else:
         return k*FibCustom(n-2, k) + FibCustom(n-1, k)
 
-print(FibCustom(28,5))
+def FibMortal(n, m):
+    # each pair of rabbits reaches maturity in one month
+    # produces a single pair of offspring each subsequent month
+    # rabbits die out after a fixed number of months (m)
+
+    if n == 1 or n == 2:
+        return 1
+    elif n == 0:
+        return 0
+    else:
+        return Fib(n-1) + Fib(n-2) - (Fib(n)-(m+1))
+
+print(Fib(23))
